f0851f5fd41da147193296afd024e5dd616cafd1
Séverine Noël <severine.noel94@gmail.com>
2017-10-03 20:47:18 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Very soon sur ton écran">
+    <meta name="keywords"
+          content="ovaires, ovaire, féministe, feminisme, je m'en bats les ovaires, femme, twitter, digital"/>
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@GiruFrei"/>
+    <meta name="twitter:title" content="Je m'en bats les ovaires"/>
+    <meta name="twitter:description" content="Very soon sur ton écran"/>
+    <meta name="twitter:image" content=""/>
+    <!-- og -->
+    <meta property="og:title" content="Je m'en bats les ovaires"/>
+    <meta property="og:description" content="Very soon sur ton écran"/>
+    <meta property="og:image" content=""/>
+
+    <base href="https://jemenbatslesovaires.fr/"/>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Je m'en bats les ovaires</title>
+</head>
+<body>
+<p>
+    La flemme est présente
+    <span>
+    Jemenbatslesovaires.fr
+  </span>
+    &mdash; Very soon sur ton écran &mdash;
+</p>
+<footer>
+    Made with love by <a href="https://twitter.com/GiruFrei">@GiruFrei</a>
+</footer>
+</body>
